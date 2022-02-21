04f69bbee94b9700b0c28ed903550b074b6f7b3d
Rian Zenki Nacazato <rian.zenki@gmail.com>
2022-02-20 21:15:03 -0300

    Projeto inicial concluido

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { FiSearch } from 'react-icons/fi'
+import { useState } from 'react'
+import api from './services/api'
+import './styles.css'
 
 function App() {
+
+  const [input, setInput] = useState('')
+  const [cep, setCep] = useState({})
+
+  const handleSearch = async () => {
+
+      if(input) {
+          try{
+              const response = await api.get(`${input}/json`)
+              setCep(response.data)
+              setInput('')
+          }catch {
+              alert('Erro na busca')
+              setInput('')
+          }
+      }
+      else {
+          console.log('Preencha com algum CEP')
+          return
+      }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+        <h1 className="title">Buscador CEP</h1>
+
+        <div className="containerInput">
+            <input 
+                type="text"
+                placeholder="Digite seu CEP"
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
+            />
+            
+            <button className="buttonSearch" onClick={handleSearch}>
+                <FiSearch size={25} color="#FFF" />
+            </button>
+        </div>
+
+        {Object.keys(cep).length > 1 && (
+            <main className="main">
+                <h2>CEP: {cep.cep}</h2>
+
+                <span>Logradouro: {cep.logradouro}</span>
+                <span>Complemento: {cep.complemento}</span>
+                <span>{cep.bairro}</span>
+                <span>{`${cep.localidade} - ${cep.uf}`}</span>
+            </main>
+        )}
+
     </div>
   );
 }
